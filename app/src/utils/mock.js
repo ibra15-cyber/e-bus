ee324c9f90ff61e23ce3d45c91173b9b33f0569b
ibra15-cyber <iasafianu@st.ug.edu.gh>
2022-06-02 17:04:20 +0000

    mini project

---
+++ app/src/utils/mock.js
@@ -0,0 +1,89 @@
+export const addressData = [
+  {
+    id: 1,
+    name: "Kumasi",
+  },
+  {
+    id: 2,
+    name: "Tamale",
+  },
+  {
+    id: 3,
+    name: "Bolgatanga",
+  },
+  {
+    id: 4,
+    name: "Sunyani",
+  },
+  {
+    id: 5,
+    name: "Cape Coast",
+  },
+  {
+    id: 6,
+    name: "Tarkwa",
+  },
+  {
+    id: 7,
+    name: "Koforidua",
+  },
+  {
+    id: 8,
+    name: "Obuasi",
+  },
+  {
+    id: 9,
+    name: "Tema",
+  },
+  {
+    id: 10,
+    name: "Bawku",
+  },
+  {
+    id: 11,
+    name: "Winneba",
+  },
+  {
+    id: 12,
+    name: "Mampong",
+  },
+  {
+    id: 13,
+    name: "Kintampo",
+  },
+];
+
+export const busData = [
+    {
+        id: 1,
+        title: "Ebus Delux",
+        image: "https://www.yatranepal.com/images/dhangadi-bus/dhangadi-bus-02.jpg",
+        seats: 14,
+        price: "Ghs 1600",
+        time: "1PM"
+    },
+    {
+        id: 1,
+        title: "Ebus Air Condition",
+        image: "https://worldhimalayandestination.com/wp-content/uploads/2019/06/tourist-bus-825x450.jpg",
+        seats: 11,
+        price: "Ghs 2000",
+        time: "3PM"
+    },
+    {
+        id: 1,
+        title: "Ebus Suspension",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSObRM0s5pOI2ODbdshb0hchMlhRO4SXxp32MrxVUA6jMzUelUU&usqp=CAU",
+        seats: 10,
+        price: "Ghs 1400",
+        time: "5PM"
+    },
+    {
+        id: 1,
+        title: "Ebus Normal",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQvYXA4bxfAD5ByqAbc7je7DIOm1I9AyTZcoh3t8Ya2KT0WhOvT&usqp=CAU",
+        seats: 12,
+        price: "Ghs 1000",
+        time: "2PM"
+    },
+]
